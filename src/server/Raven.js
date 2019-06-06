f5260ca2676d241e6f0391ab34ecfe6ce7481036
Rhys Howell <Anemy@users.noreply.github.com>
2019-06-06 17:16:20 -0400

    override error handling for server because I lost the raven key :)

--- src/server/Raven.js
+++ src/server/Raven.js
@@ -1,20 +1,21 @@
 import Environment from '../shared/Environment'; 
-import Raven from 'raven';
+// import Raven from 'raven';
 
 export default {
   capture: () => {
-    if (Environment.isProd()) {
-      if (!process.env.RAVEN_KEY) {
-        throw new Error('ERROR: Raven key not defined in environment.');
-      } else {
-        Raven.config(process.env.RAVEN_KEY).install();
-      }
-    }
+//     if (Environment.isProd()) {
+//       if (!process.env.RAVEN_KEY) {
+//         throw new Error('ERROR: Raven key not defined in environment.');
+//       } else {
+//         Raven.config(process.env.RAVEN_KEY).install();
+//       }
+//     }
   },
 
   logError: (err) => {
-    if (Environment.isProd()) {
-      Raven.captureException(err);
-    }
+//     if (Environment.isProd()) {
+//       Raven.captureException(err);
+//     }
+    console.log('an error occured': err);
   }
 };
